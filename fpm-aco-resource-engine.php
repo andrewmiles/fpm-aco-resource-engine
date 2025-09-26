14ba39485525e8871ac7a67be1a82ac154f39572
fpmam <143112821+fpmam@users.noreply.github.com>
2025-09-26 12:36:33 +0100

    chore: Bump version to 1.15.1

--- fpm-aco-resource-engine.php
+++ fpm-aco-resource-engine.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     1 - FPM - ACO Resource Engine
  * Description:     Core functionality for the ACO Resource Library, including failover, sync, and content models.
- * Version:         1.15.0
+ * Version:         1.15.1
  * GitHub Plugin URI: andrewmiles/fpm-aco-resource-engine
  * Primary Branch: main
  * Author:          FPM
